2fca943280cd0d4e0214e44a103d0e790b82799c
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-21 13:28:35 -0500

    Updates to server.js.

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require(); // require the controller 
+
+app.use(routes);
+
+app.listen(PORT, function() {
+
+    console.log("Listening on port " + PORT);
+});
