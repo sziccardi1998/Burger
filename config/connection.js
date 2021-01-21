74a71d2abad7b6ddbbe87cf1cae5bcbb90f72223
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-21 15:17:19 -0500

    Updated server.js and connection.js.

--- config/connection.js
+++ config/connection.js
@@ -8,4 +8,13 @@ const connection = mysql.createConnection({
     database: "employee_DB" 
 });
 
+// create the connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
