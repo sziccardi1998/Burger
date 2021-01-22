e5bf1b6863b8d9941f11d43620d23d41d7ab1fd2
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-22 09:06:33 -0500

    printQuestionMark added to orm.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,14 @@
+// bring in mysql connection
 var connection = require('./connection');
 
+// create a helper function that makes sql syntax easier
+function printQuestionMark(num) {
+    let holder = [];
+    for (let i = 0; i < num; i++) {
+        holder.push("?");
+    }
+    return holder.toString();
+}
 
 
 const selectAll = () => {
