44c17a56a4ffe0f702ea17dbe80f87c1a707c900
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-22 14:16:58 -0500

    Added post and put routes to router.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,28 @@ router.get("/", function(req, res) {
         };
         console.log(hbsObj);
         res.render("index", hbsObj);
-    })
-})
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+    
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return .res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
 
 // export the app
