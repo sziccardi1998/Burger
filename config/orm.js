b64146d62a0b0a9803751c2662b079bf1e287dc1
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-22 09:13:08 -0500

    Additional helper function added to orm.

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,20 @@ function printQuestionMark(num) {
     return holder.toString();
 }
 
+// create a helper function that converts key/value pairs
+function objectToSQL(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value)
+        }
+    }
+    return arr.toString();
+}
 
 const selectAll = () => {
 
