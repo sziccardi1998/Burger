939ac01050eba23396259ca730b3262237b232a1
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-22 09:28:52 -0500

    Functions added to orm.

--- config/orm.js
+++ config/orm.js
@@ -25,15 +25,25 @@ function objectToSQL(ob) {
     return arr.toString();
 }
 
-const selectAll = () => {
-
+var orm = {
+    selectAll: function(table, cb) {
+        var dbQuery = "SELECT * FROM " + table + ";";
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " + table + " (" + cols.toString() + ") VALUES (" + printQuestionMark(vals.length) + ") ";
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    updateOne: function
 }
-
-const insertOne = () => {
-
-}
-
-const updateOne = () => {
-
-}
-
