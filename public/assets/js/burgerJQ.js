9af48bf950b09624697b9b80cc1947bc9a742603
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-25 17:32:00 -0500

    Trouble shooting changes.

--- public/assets/js/burgerJQ.js
+++ public/assets/js/burgerJQ.js
@@ -1,15 +1,16 @@
 $(function() {
     // handle the submission of burgers
-    $(".create-form").on("submit", function(event) {
+    $("#text-enter-button").on("click", function(event) {
         // prevent form from actually submitting
         event.preventDefault();
         
         // bring in new uneaten burger
         var createBurger = {
-            burger_name: $("enter_text").val().trim(),
+            burger_name: $("#enter_text").val(),
             devoured: 0
         };
 
+        // call api route to handle the addition of the new burger
         $.ajax("/api/burgers", {
             type: "POST",
             data: createBurger
@@ -18,23 +19,26 @@ $(function() {
         });
     });
 
+    // handle burgers being clicked on
     $(".eatBurger").on("click", function(event) {
         event.preventDefault();
         
         // get the id of the clicked burger
-        let id = $(this).data("id");
+        let id = $(this).parent().parent().data("id");
         console.log(id);
         // change the boolean value of the burger
         let devouredTrue = {
             devoured: 1
         };
 
-        // create an ajax call
-        $.ajax("/api/burger/" + id, {
+        // create an ajax call to make changes to the burger
+        $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: devouredTrue
         }).then(function() {
             location.reload();
+
         });
     });
-})
+
+});
